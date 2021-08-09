9f4ee89a9bc060f96c891bee26acba143f58127e
chrisbyd <chenyongbiao0319@sina.com>
2021-08-09 10:17:49 +0800

    running long-tailed hashing

--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
 python run.py --root ./data_raw/cifar-100/cifar-100-IF50   --code-length 16 \
-              --num-classes 100 --evaluate-interval 50 \
-              --dataset cifar-100-IF50   --gpu 0
+              --num-classes 100 --evaluate-interval 20 \
+              --dataset cifar-100-IF50   --gpu 0  \
+              --max-iter 3000
