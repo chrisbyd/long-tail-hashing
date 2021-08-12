0d3891dc140e39d835e8f43b75a53c5f50a4ad99
chrisbyd <chenyongbiao0319@sina.com>
2021-08-12 14:29:57 +0800

    modify batch size

--- train.sh
+++ train.sh
@@ -2,4 +2,4 @@ python run.py --root ./data_raw/cifar-100/cifar-100-IF50   --code-length 32 \
               --num-classes 100 --evaluate-interval 25 \
               --dataset cifar-100-IF50   --gpu 0  \
               --max-iter 2000    \
-              --batch-size 64
+              --batch-size 8
